6cb321ae683d6e5712232e5863d447e08354f54b
Dylan McKee <dylan@djmckee.co.uk>
2016-02-06 09:02:30 +0000

    Removed tutorial file for transition to SQL database.

--- app/src/main/java/com/projectcrescendo/projectcrescendo/TutorialManager.java
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/TutorialManager.java
@@ -18,10 +18,6 @@ import org.json.*;
  */
 public class TutorialManager {
 
-    /**
-     * A constant containing the reference to the json file containing the tutorials.
-     */
-    private static final int TUTORIALS_FILE = R.raw.tutorials;
 
     /**
      * The list of tutorials.
@@ -32,44 +28,7 @@ public class TutorialManager {
      * A public constructor to instantiate the tutorials from the JSON file containing them.
      */
     public TutorialManager(Context context) {
-        // Instantiate the Tutorials from tutorials.json
-        String tutorialFileString = FileHandler.stringFromFile(context, TUTORIALS_FILE);
 
-        try {
-            // Parse tutorials.json into this array!!!!
-            JSONArray array = new JSONArray(tutorialFileString);
-
-            // Loop through the array, creating a Tutorial instance for each entry...
-            for (int i = 0; i < array.length(); i++) {
-                JSONObject object = array.getJSONObject(i);
-
-                // Create tutorial...
-                String tutorialTitle = object.getString("title");
-                Tutorial tutorial = new Tutorial(tutorialTitle);
-
-                // Get instructions too...
-                JSONArray instructions = object.getJSONArray("instructions");
-
-                for (int j = 0; j < instructions.length(); j++) {
-                    // Create the instruction object...
-                    JSONObject instructionObject = instructions.getJSONObject(j);
-                    String instructionText = instructionObject.getString("text");
-                    Instruction instruction = new Instruction(instructionText);
-
-                    // And add it to the tutorial's instruction list...
-                    tutorial.addInstruction(instruction);
-
-                }
-
-                // Then add the tutorial to the list...
-                tutorialsList.add(tutorial);
-
-
-            }
-
-        } catch (JSONException e) {
-
-        }
 
     }
 
