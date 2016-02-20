01acf0fd3e1ae3ac392fe9817eeac809649929ae
Dylan McKee <dylan@djmckee.co.uk>
2016-02-20 13:58:18 +0000

    Added Jordan's work. http://gifs.parkermoo.re/win/yes-github-yissssss.gif

---
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/GridHolder.java
@@ -0,0 +1,75 @@
+package com.projectcrescendo.projectcrescendo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import java.util.ArrayList;
+
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+/**
+ * Created by Jordan on 06/02/2016.
+ class to create grid holders for boxes.
+ **/
+public class GridHolder extends ActionBarActivity {
+
+    private GridviewAdapter mAdapter;
+    private GridView gridView;
+    private ArrayList<String> listNotes;
+    private ArrayList<Button> listvol;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_music);
+
+        noteList();
+
+        // prepared arraylist and passed it to the Adapter class
+        mAdapter = new GridviewAdapter(this,listNotes, listvol);
+
+        // Set custom adapter to gridview
+        gridView = (GridView) findViewById(R.id.gridView1);
+        gridView.setAdapter(mAdapter);
+
+        // Implement On Item click listener
+
+
+    }
+
+    public void noteList()
+    {
+        listNotes = new ArrayList<String>();
+
+        listNotes.add("No Notes");
+        listNotes.add("No Notes");
+        listNotes.add("No Notes");
+        listNotes.add("No Notes");
+        listNotes.add("No Notes");
+        listNotes.add("No Notes");
+        listNotes.add("No Notes");
+        listNotes.add("No Notes");
+        listNotes.add("No Notes");
+        listNotes.add("No Notes"); //notes to edit text
+
+        listvol = new ArrayList<Button>();
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+        listvol.add((Button)findViewById(R.id.button1));
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/GridviewAdapter.java
@@ -0,0 +1,82 @@
+package com.projectcrescendo.projectcrescendo;
+
+/**
+ * Created by Jordan on 11/02/2016.
+ */
+
+import java.util.ArrayList;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.projectcrescendo.projectcrescendo.R;
+
+public class GridviewAdapter extends BaseAdapter
+{
+    private ArrayList<String> notes;
+    private ArrayList<Button> vol;
+    private Activity activity;
+
+    public GridviewAdapter(Activity activity,ArrayList<String> noteslist, ArrayList<Button> volumeList) {
+        super();
+        this.notes = noteslist;
+        this.vol = volumeList;
+        this.activity = activity;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return notes.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        // TODO Auto-generated method stub
+        return notes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public static class ViewHolder
+    {
+        public Button changevol;
+        public EditText txteditnotes;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        ViewHolder view;
+        LayoutInflater inflator = activity.getLayoutInflater();
+
+        if(convertView==null)
+        {
+            view = new ViewHolder();
+            convertView = inflator.inflate(R.layout.activity_music, null);
+
+            view.txteditnotes = (EditText) convertView.findViewById(R.id.textView1);
+            view.changevol = (Button) convertView.findViewById(R.id.button1);
+
+            convertView.setTag(view);
+        }
+        else
+        {
+            view = (ViewHolder) convertView.getTag();
+        }
+
+        view.txteditnotes.setText(notes.get(position));
+
+        return convertView;
+    }
+}
