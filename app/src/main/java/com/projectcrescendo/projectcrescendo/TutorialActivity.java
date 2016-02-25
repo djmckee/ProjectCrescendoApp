bd2a1b157533093c836eb4e5f3f9e02944a747f9
Dylan McKee <dylan@djmckee.co.uk>
2016-02-25 11:28:36 +0000

    Refactored spinner code to match Java style guidelines.

--- app/src/main/java/com/projectcrescendo/projectcrescendo/TutorialActivity.java
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/TutorialActivity.java
@@ -1,14 +1,9 @@
 package com.projectcrescendo.projectcrescendo;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.projectcrescendo.projectcrescendo.models.Intonation;
@@ -16,13 +11,11 @@ import com.projectcrescendo.projectcrescendo.models.Note;
 
 import java.util.ArrayList;
 import java.util.List;
-import android.app.Activity;
-import android.view.View.OnClickListener;
+
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 
-// got code from http://prasans.info/2011/03/add-edittexts-dynamically-and-retrieve-values-android/
 public class TutorialActivity extends ActionBarActivity implements CustomAdapterListener, AddNoteFragmentListener  {
 
     GridView gridView;
@@ -30,7 +23,10 @@ public class TutorialActivity extends ActionBarActivity implements CustomAdapter
     ArrayList prgmName;
     public static String [] prgmNameList={"c","a","b","f","e","b","c","a"};
 
-    Spinner time_signature_r1, time_signature_r2, time_signature_l1, time_signature_l2;
+    Spinner timeSignatureR1;
+    Spinner timeSignatureR2;
+    Spinner timeSignatureL1;
+    Spinner timeSignatureL2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,21 +56,26 @@ public class TutorialActivity extends ActionBarActivity implements CustomAdapter
 
     public void addItemsOnSpinner() {
 
-        time_signature_r1 = (Spinner) findViewById(R.id.right_hand_time_signature_1);
-        time_signature_r2 = (Spinner) findViewById(R.id.right_hand_time_signature_2);
-        time_signature_l1 = (Spinner) findViewById(R.id.left_hand_time_signature_1);
-        time_signature_l2 = (Spinner) findViewById(R.id.left_hand_time_signature_2);
+        // Ambrose: got code from http://prasans.info/2011/03/add-edittexts-dynamically-and-retrieve-values-android/ for the Spinner UI
+        timeSignatureR1 = (Spinner) findViewById(R.id.right_hand_time_signature_1);
+        timeSignatureR2 = (Spinner) findViewById(R.id.right_hand_time_signature_2);
+        timeSignatureL1 = (Spinner) findViewById(R.id.left_hand_time_signature_1);
+        timeSignatureL2 = (Spinner) findViewById(R.id.left_hand_time_signature_2);
+
         List<String> list = new ArrayList<String>();
         list.add("2");
         list.add("3");
         list.add("4");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        time_signature_r1.setAdapter(dataAdapter);
-        time_signature_r2.setAdapter(dataAdapter);
-        time_signature_l1.setAdapter(dataAdapter);
-        time_signature_l2.setAdapter(dataAdapter);
+
+        timeSignatureR1.setAdapter(dataAdapter);
+        timeSignatureR2.setAdapter(dataAdapter);
+        timeSignatureL1.setAdapter(dataAdapter);
+        timeSignatureL2.setAdapter(dataAdapter);
+
     }
 
 
