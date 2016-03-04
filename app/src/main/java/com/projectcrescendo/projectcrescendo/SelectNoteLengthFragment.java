ace77b1c69ddcc153bd08b17a51a4a1ca0044df7
Dylan McKee <dylan@djmckee.co.uk>
2016-03-04 12:31:39 +0000

    Javadocs

--- app/src/main/java/com/projectcrescendo/projectcrescendo/SelectNoteLengthFragment.java
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/SelectNoteLengthFragment.java
@@ -16,24 +16,57 @@ import com.projectcrescendo.projectcrescendo.models.Note;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * An interface to handle callbacks from the SelectNoteLengthFragment.
+ *
+ * Created by Dylan McKee on 27/02/2016
+ */
 interface SelectNoteLengthFragmentCallbackListener {
+    /**
+     * This method is called when the user has selected a length value from the length
+     * selection fragment list, and the fragment has dismissed itself from the screen.
+     * @param fragment the note length selection fragment.
+     * @param length the length for the note, in terms of beats.
+     */
     void noteLengthSelectedFromFragment(SelectNoteLengthFragment fragment, double length);
 
 }
 
 /**
+ * A fragment that contains a list of possible note lengths so that the user can select an note length
+ * for the new note begin added in hte AddNoteFragment.
+ *
+ * Once selected, the Intonation is relayed via a callback to the fragment that
+ * presented it via the SelectNoteLengthFragmentCallbackListener.
+ *
+ * I looked at the tutorial at http://www.tutorialsbuzz.com/2014/06/android-dialogfragment-listview.html
+ * and used their examples in the creation of this fragment.
+ *
  * Created by Dylan McKee on 27/02/2016
- * <p>
- * Based off of the SelectIntonationFragment, using tutorial example http://www.tutorialsbuzz.com/2014/06/android-dialogfragment-listview.html.
+ *
  */
 public class SelectNoteLengthFragment extends DialogFragment implements
         OnItemClickListener {
 
-
+    /**
+     * A list view containing the possible lengths for the note that the user can select from.
+     */
     private ListView lengthListView;
 
+    /**
+     * A callback listener that implements the SelectNoteLengthFragmentCallbackListener, so that
+     * the fragment/activity presenting this fragment can receive a callback and know when the
+     * length has been selected.
+     */
     private SelectNoteLengthFragmentCallbackListener listener;
 
+    /**
+     * On load, this method inflates the fragment from XML.
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -45,6 +78,11 @@ public class SelectNoteLengthFragment extends DialogFragment implements
         return view;
     }
 
+    /**
+     * After the fragments XML has been loaded, this method populates the list view with the possible
+     * lengths for the note that the user can select from.
+     * @param savedInstanceState
+     */
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
 
@@ -74,6 +112,15 @@ public class SelectNoteLengthFragment extends DialogFragment implements
 
     }
 
+    /**
+     * When an length in the list of lengths is selected by the user, this method calls the listener
+     * to let it know of the new selection, and then once this callback has been carried out it
+     * dismisses the lenght selection fragment from the screen.
+     * @param parent
+     * @param view
+     * @param position
+     * @param id
+     */
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position,
                             long id) {
@@ -91,10 +138,18 @@ public class SelectNoteLengthFragment extends DialogFragment implements
 
     }
 
+    /**
+     * Returns the callback listener.
+     * @return the callback listener.
+     */
     public SelectNoteLengthFragmentCallbackListener getListener() {
         return listener;
     }
 
+    /**
+     * Sets the callback listener
+     * @param listener the callback listener that is presenting this fragment
+     */
     public void setListener(SelectNoteLengthFragmentCallbackListener listener) {
         this.listener = listener;
     }
