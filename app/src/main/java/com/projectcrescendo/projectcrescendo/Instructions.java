c0c993cef806388e4ecf4805dd68c6f7953fb8c3
Ambrose <ambrosesch@gmail.com>
2016-04-03 17:45:45 +0100

    modify instruction page into landscape view, resize the instruction

--- app/src/main/java/com/projectcrescendo/projectcrescendo/Instructions.java
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/Instructions.java
@@ -14,4 +14,5 @@ public class Instructions extends ActionBarActivity {
         setContentView(R.layout.activity_instructions);
     }
 
+
 }
---
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/ProportionalImageView.java
@@ -0,0 +1,35 @@
+package com.projectcrescendo.projectcrescendo;
+
+import android.widget.ImageView;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+/**
+ * Created by Ambrose Suen on 3/4/2016.
+ * Reference from http://www.ryadel.com/en/android-proportionally-stretch-imageview-fit-whole-screen-width-maintaining-aspect-ratio/
+ */
+public class ProportionalImageView extends ImageView {
+    public ProportionalImageView(Context context) {
+        super(context);
+    }
+
+    public ProportionalImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProportionalImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Drawable d = getDrawable();
+        if (d != null) {
+            int w = MeasureSpec.getSize(widthMeasureSpec);
+            int h = w * d.getIntrinsicHeight() / d.getIntrinsicWidth();
+            setMeasuredDimension(w, h);
+        }
+        else super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
