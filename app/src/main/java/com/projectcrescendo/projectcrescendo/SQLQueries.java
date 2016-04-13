100b8cba663abd30a7f7d53713ba05b0593c4b5e
Dylan McKee <dylan@djmckee.co.uk>
2016-04-13 15:25:06 +0100

    Stave writing and reading to/from the database.

--- app/src/main/java/com/projectcrescendo/projectcrescendo/SQLQueries.java
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/SQLQueries.java
@@ -26,6 +26,51 @@ public final class SQLQueries {
     public static final String SELECT_ALL_NOTE_NAMES = "SELECT * FROM note_names";
 
     /**
+     * Select all staves from the stave table.
+     */
+    public static final String SELECT_ALL_STAVES = "SELECT * FROM stave";
+
+    /**
+     * Select all bars with the specified stave ID.
+     */
+    public static final String SELECT_BARS_WITH_STAVE_ID = "SELECT * from bar WHERE stave_id = %d";
+
+    /**
+     * Select all beats with the specified bar ID.
+     */
+    public static final String SELECT_BEATS_WITH_BAR_ID = "SELECT * from beat WHERE bar_id = %d";
+
+    /**
+     * Select all notes with the specified beat ID.
+     */
+    public static final String SELECT_NOTES_WITH_BEAT_ID = "SELECT * from beat WHERE beat_id = %d";
+
+    /**
+     * Select intonation with the specified intonation ID.
+     */
+    public static final String SELECT_INTONATION_WITH_ID = "SELECT * from intonation WHERE id = %d";
+
+    /**
+     * Select note name with the specified note name ID.
+     */
+    public static final String SELECT_NOTE_NAME_WITH_ID = "SELECT * from note_names WHERE id = %d";
+
+    /**
+     * Select note name with the specified note name string.
+     */
+    public static final String SELECT_NOTE_NAME_WITH_NAME = "SELECT * from note_names WHERE name = %s";
+
+    /**
+     * Select time signature with the specified numerator and denominator.
+     */
+    public static final String SELECT_TIME_SIGNATURE_WITH_NUMBERS = "SELECT * from time_signature WHERE first = %d AND second = %d";
+
+    /**
+     * Select time signature with the specified id.
+     */
+    public static final String SELECT_TIME_SIGNATURE_WITH_ID = "SELECT * from time_signature WHERE id = %d";
+
+    /**
      * Select all tutorials from the tutorials table.
      */
     public static final String SELECT_ALL_TUTORIALS = "SELECT * FROM tutorials";
@@ -56,6 +101,11 @@ public final class SQLQueries {
     public static final String NOTE_ENTIRETY_NAME_COLUMN = "name";
 
     /**
+     * The name of the id of the Time Signature entirety.
+     */
+    public static final String TIME_SIGNATURE_ID_COLUMN = "id";
+
+    /**
      * The name of the first field of the Time Signature entirety.
      */
     public static final String TIME_SIGNATURE_ENTIRETY_FIRST_COLUMN = "first";
@@ -64,5 +114,77 @@ public final class SQLQueries {
      * The name of the second field of the Time Signature entirety.
      */
     public static final String TIME_SIGNATURE_ENTIRETY_SECOND_COLUMN = "second";
+
+    /**
+     * The name of the table containing stave instances.
+     */
+    public static final String STAVE_TABLE_NAME = "stave";
+
+    /**
+     * The name of the table containing bar instances.
+     */
+    public static final String BAR_TABLE_NAME = "bar";
+
+    /**
+     * The name of the table containing beat instances.
+     */
+    public static final String BEAT_TABLE_NAME = "beat";
+
+    /**
+     * The name of the table containing note instances.
+     */
+    public static final String NOTE_TABLE_NAME = "notes";
+
+    /**
+     * The name of the column on the Stave entirety that holds the 'time signature ID'
+     */
+    public static final String STAVE_TIME_SIGNATURE_ID_COLUMN = "time_signature_id";
+
+    /**
+     * The name of the id column of the Stave entirety.
+     */
+    public static final String STAVE_ID_COLUMN = "id";
+
+    /**
+     * The name of the name column of the Stave entirety.
+     */
+    public static final String STAVE_NAME_COLUMN = "name";
+
+    /**
+     * The name of the id column of the Bar entirety.
+     */
+    public static final String BAR_ID_COLUMN = "id";
+
+    /**
+     * The name of the number column of the Bar entirety.
+     */
+    public static final String BAR_NUMBER_COLUMN = "number";
+
+    /**
+     * The name of the stave id column of the Bar entirety.
+     */
+    public static final String BAR_STAVE_ID_COLUMN = "stave_id";
+
+    /**
+     * The name of the id column of the Beat entirety.
+     */
+    public static final String BEAT_ID_COLUMN = "id";
+
+    /**
+     * The name of the number column of the Beat entirety.
+     */
+    public static final String BEAT_NUMBER_COLUMN = "number";
+
+
+    public static final String BEAT_BAR_ID_COLUMN = "bar_id";
+    public static final String BEAT_DYNAMIC_COLUMN = "dynamic_id";
+    public static final String NOTE_NAMES_NAME_ID_COLUMN = "id";
+    public static final String NOTE_NAME_ID_COLUMN = "name_id";
+    public static final String NOTE_INTONATION_ID_COLUMN = "intonation_id";
+    public static final String NOTE_BEAT_ID_COLUMN = "beat_id";
+    public static final String NOTE_LENGTH_COLUMN = "length";
+    public static final String NOTE_NAMES_NAME_COLUMN = "name";
+    public static final String STAVE_DELETE_WHERE_CLAUSE = STAVE_ID_COLUMN + "=";
+
 
 }
