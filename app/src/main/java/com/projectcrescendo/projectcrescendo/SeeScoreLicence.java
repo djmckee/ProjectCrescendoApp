887154383b4d397faeff269112403838ef9c50ce
Dylan McKee <dylan@djmckee.co.uk>
2016-05-04 20:01:06 +0100

    Licence

---
+++ app/src/main/java/com/projectcrescendo/projectcrescendo/SeeScoreLicence.java
@@ -0,0 +1,81 @@
+/**
+ * We obtained this Licence file from Dolphin Computing Ltd. to allow us to use their
+ * SeeScore SDK Library for educational purposes only.
+ * <p/>
+ * SeeScore For Android
+ * Dolphin Computing http://www.dolphin-com.co.uk
+ * <p/>
+ * SeeScore For Android
+ * Dolphin Computing http://www.dolphin-com.co.uk
+ * <p/>
+ * SeeScore For Android
+ * Dolphin Computing http://www.dolphin-com.co.uk
+ * <p/>
+ * SeeScore For Android
+ * Dolphin Computing http://www.dolphin-com.co.uk
+ * <p/>
+ * SeeScoreLib Key for ProjectCrescendo
+ * <p/>
+ * IMPORTANT! This file is for ProjectCrescendo only.
+ * It must be used only for the application for which it is licensed,
+ * and must not be released to any other individual or company.
+ * Please keep it safe, and make sure you don't post it online or email it.
+ * Keep it in a separate folder from your source code, so that when you backup the code
+ * or store it in a source management system, the key is not included.
+ * <p/>
+ * SeeScore For Android
+ * Dolphin Computing http://www.dolphin-com.co.uk
+ * <p/>
+ * SeeScoreLib Key for ProjectCrescendo
+ * <p/>
+ * IMPORTANT! This file is for ProjectCrescendo only.
+ * It must be used only for the application for which it is licensed,
+ * and must not be released to any other individual or company.
+ * Please keep it safe, and make sure you don't post it online or email it.
+ * Keep it in a separate folder from your source code, so that when you backup the code
+ * or store it in a source management system, the key is not included.
+ * <p/>
+ * SeeScore For Android
+ * Dolphin Computing http://www.dolphin-com.co.uk
+ * <p/>
+ * SeeScoreLib Key for ProjectCrescendo
+ * <p/>
+ * IMPORTANT! This file is for ProjectCrescendo only.
+ * It must be used only for the application for which it is licensed,
+ * and must not be released to any other individual or company.
+ * Please keep it safe, and make sure you don't post it online or email it.
+ * Keep it in a separate folder from your source code, so that when you backup the code
+ * or store it in a source management system, the key is not included.
+ */
+/**
+ * SeeScore For Android
+ * Dolphin Computing http://www.dolphin-com.co.uk
+ */
+/**
+ *  SeeScoreLib Key for ProjectCrescendo
+ *
+ *  IMPORTANT! This file is for ProjectCrescendo only.
+ *  It must be used only for the application for which it is licensed,
+ *  and must not be released to any other individual or company.
+ *  Please keep it safe, and make sure you don't post it online or email it.
+ *  Keep it in a separate folder from your source code, so that when you backup the code
+ *  or store it in a source management system, the key is not included.
+ *
+ */
+
+package com.projectcrescendo.projectcrescendo;
+
+import uk.co.dolphin_com.sscore.SScoreKey;
+
+/**
+ * The licence key to enable features in SeeScoreLib supplied by Dolphin Computing
+ */
+
+class SeeScoreLicence {
+    // licence keys: draw, contents, transpose, play_data, item_colour, multipart, android, midi_out
+    private static final int[] keycap = {***REMOVED***};
+    private static final int[] keycode = {***REMOVED***};
+
+    public static final SScoreKey SeeScoreLibKey = new SScoreKey("ProjectCrescendo", keycap, keycode);
+
+}
